483b43c72a451e4d17bbecfd4b79ec2ec5563db9
unknown <eduardtira02@gmail.com>
2022-08-11 19:53:36 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,27 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 
-const express = require('express');
-const app = express();
-
-
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  )
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "POST, PUT, PATCH, GET, DELETE"
-    )
-    return res.status(200).json({})
-  }
-  next()
-})
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
